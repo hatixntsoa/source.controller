ad6b9e07bd73208bfe7d32cae5bc43c34f3421e2
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-11-23 19:49:15 +0300

    refactor: concise check_user function

--- utils/check_user.sh
+++ utils/check_user.sh
@@ -5,26 +5,14 @@ function is_a_github_user {
 	username="$1"
 
 	# Check if username is empty
-	if [ -z "$username" ]; then
-		return 1
-	fi
+	[ -z "$username" ] && return 1
 
 	# Build the API URL
 	url="https://api.github.com/users/$username"
 
-	# Use wget to capture the response (redirecting output to a variable)
-	# wget by default outputs content, so we use the -q (quiet) option to suppress it
-	# -O- option specifies that the downloaded content should be written
-	# to standard output (stdout) instead of a file.
+	# Checking response from the URL
 	response=$(wget -qO- --no-check-certificate "$url")
 
-	# Check if there is no output
-	# meaning it is not found
-	if [ -z "$response" ]; then
-		# Not Found
-		return 1
-	else
-		# Found
-		return 0
-	fi
+	# If we receive a response then the user exists
+	[ -n "$response" ] && return 0 || return 1
 }
