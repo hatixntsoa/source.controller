2144c2b7db57f330d840a320deed39062d8993d1
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-11-26 15:15:41 +0300

    update: Check repo URL

--- utils/check_repo.sh
+++ utils/check_repo.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 function is_a_github_repo {
-  gh repo view "$1" >/dev/null 2>&1
-  return $?
+	repo_url="$1"
+
+	# Check if username is empty
+	[ -z "$repo_url" ] && return 1
+
+	# Build the API URL
+	url="https://api.github.com/repos/$repo_url"
+
+	# Checking response from the URL
+	response=$(wget -qO- --no-check-certificate "$url")
+
+	# If we receive a response then the user exists
+	[ -n "$response" ] && return 0 || return 1
 }
