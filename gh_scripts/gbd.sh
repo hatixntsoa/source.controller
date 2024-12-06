152f9a6fe41bbaac9c1c7eb8e4f520db210c2146
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-12-06 20:21:49 +0000

    refactor[gbd]: Cleaner code

--- gh_scripts/gbd.sh
+++ gh_scripts/gbd.sh
@@ -17,123 +17,132 @@ function gbd {
 	if [ $# -eq 1 ]; then
 		if [ "$1" = "$default_branch" ]; then
 			echo "${BOLD} Fatal ! Cannot Delete the Default Branch "
-		elif ! git show-ref --verify --quiet "refs/heads/$1" &>/dev/null; then
-			echo "${BOLD} Fatal ! Branch ${GREEN}$1 ${RESET_COLOR}doesn't exist ${RESET}"
-		else
-			# this to check if we want to delete the remote branch too
-			check_delete_remote_branch() {
-				if [ "$current_branch" = "$default_branch" ]; then
-					echo "${BOLD} Fatal ! Cannot Delete the Default Branch "
-				else
-					printf "${BOLD}${RESET_COLOR}Delete remote branch${GREEN} "$current_branch"${RESET_COLOR} ? (y/n) ${RESET}"
-					read delete_remote_branch
-					echo ${RESET}
-					if [ "$delete_remote_branch" = "y" ]; then
-						git push origin --delete "$current_branch"
-					elif [ "$delete_remote_branch" = "n" ]; then
-						return 0
-					else
-						check_delete_remote_branch
-					fi
-				fi
-			}
-
-			check_delete_branch() {
-				branch_name="$1"
-
-				printf "${BOLD}${RESET_COLOR}Delete branch${GREEN} "$branch_name"${RESET_COLOR} ? (y/n) ${RESET}"
-				read delete_branch
-
-				if [ "$delete_branch" = "y" ]; then
-					if [ "$current_branch" != "$default_branch" ]; then
-						git checkout $default_branch >/dev/null 2>&1
-					fi
-
-					if has_remote; then
-						repo_url=$(git config --get remote.origin.url)
-						repo_owner=$(echo "$repo_url" | awk -F '[/:]' '{print $(NF-1)}')
-
-						# check if we are not the owner of the repo
-						if [ "$repo_owner" == "$current_user" ]; then
-							is_remote_branch=$(git branch -r | grep "origin/$1")
-							if [ -n "$is_remote_branch" ]; then
-								# prompt for sudo
-								# password if required
-								allow_sudo
-
-								# Check for internet connectivity to GitHub
-								if $SUDO ping -c 1 github.com &>/dev/null; then
-									check_delete_remote_branch
-								fi
-							fi
-						fi
-					fi	
-					git branch -D "$1"
-				elif [ "$delete_branch" = "n" ]; then
-					return 0
-				else
-					check_delete_branch $branch_name
-				fi
-			}
-			check_delete_branch $1
+			return 0
 		fi
-	elif [ $# -eq 0 ]; then
-		if [ "$current_branch" = "$default_branch" ]; then
-			echo "${BOLD}${RESET_COLOR} Fatal ! Cannot Delete the Default Branch "
-		else
-			check_delete_branch() {
-				printf "${BOLD}${RESET_COLOR}Delete branch${GREEN} "$current_branch"${RESET_COLOR} ? (y/n) ${RESET}"
-				read delete_branch
-				if [ "$delete_branch" = "y" ]; then
-					# TODO : Remote branch Deletion
-					check_delete_remote_branch() {
-						if [ "$current_branch" = "$default_branch" ]; then
-							echo "${BOLD}${RESET_COLOR} Fatal ! Cannot Delete the Default Branch "
-						else
-							printf "${BOLD}${RESET_COLOR}Delete remote branch${GREEN} "$current_branch"${RESET_COLOR} ? (y/n) ${RESET}"
-							read delete_remote_branch
-							echo ${RESET}
-							if [ "$delete_remote_branch" = "y" ]; then
-								git push origin --delete "$current_branch"
-							elif [ "$delete_remote_branch" = "n" ]; then
-								return 0
-							else
+
+		if ! git show-ref --verify --quiet "refs/heads/$1" &>/dev/null; then
+			echo "${BOLD} Fatal ! Branch ${GREEN}$1 ${RESET_COLOR}doesn't exist ${RESET}"
+			return 0
+		fi
+
+		# this to check if we want to delete the remote branch too
+		check_delete_remote_branch() {
+			if [ "$current_branch" = "$default_branch" ]; then
+				echo "${BOLD} Fatal ! Cannot Delete the Default Branch "
+				return 0
+			fi
+
+			printf "${BOLD}${RESET_COLOR}Delete remote branch${GREEN} "$current_branch"${RESET_COLOR} ? (y/n) ${RESET}"
+			read delete_remote_branch
+			echo ${RESET}
+
+			if [ "$delete_remote_branch" = "y" ]; then
+				git push origin --delete "$current_branch"
+			elif [ "$delete_remote_branch" = "n" ]; then
+				return 0
+			else
+				check_delete_remote_branch
+			fi
+		}
+
+		check_delete_branch() {
+			branch_name="$1"
+
+			printf "${BOLD}${RESET_COLOR}Delete branch${GREEN} "$branch_name"${RESET_COLOR} ? (y/n) ${RESET}"
+			read delete_branch
+
+			if [ "$delete_branch" = "y" ]; then
+				if [ "$current_branch" != "$default_branch" ]; then
+					git checkout $default_branch >/dev/null 2>&1
+				fi
+
+				if has_remote; then
+					repo_url=$(git config --get remote.origin.url)
+					repo_owner=$(echo "$repo_url" | awk -F '[/:]' '{print $(NF-1)}')
+
+					# check if we are not the owner of the repo
+					if [ "$repo_owner" == "$current_user" ]; then
+						is_remote_branch=$(git branch -r | grep "origin/$1")
+						if [ -n "$is_remote_branch" ]; then
+							# prompt for sudo
+							# password if required
+							allow_sudo
+
+							# Check for internet connectivity to GitHub
+							if $SUDO ping -c 1 github.com &>/dev/null; then
 								check_delete_remote_branch
 							fi
 						fi
-					}
+					fi
+				fi
 
-					git checkout "$default_branch" >/dev/null 2>&1
+				git branch -D "$1"
+			elif [ "$delete_branch" = "n" ]; then
+				return 0
+			else
+				check_delete_branch $branch_name
+			fi
+		}
 
-					if has_remote; then
-						repo_url=$(git config --get remote.origin.url)
-						repo_owner=$(echo "$repo_url" | awk -F '[/:]' '{print $(NF-1)}')
+		check_delete_branch $1
+	elif [ $# -eq 0 ]; then
+		if [ "$current_branch" = "$default_branch" ]; then
+			echo "${BOLD}${RESET_COLOR} Fatal ! Cannot Delete the Default Branch "
+			return 0
+		fi
 
-						# check if we are not the owner of the repo
-						if [ "$repo_owner" == "$current_user" ]; then
-							is_remote_branch=$(git branch -r | grep "origin/$current_branch")
-							
-							if [ -n "$is_remote_branch" ]; then
-								# prompt for sudo
-								# password if required
-								allow_sudo
+		check_delete_branch() {
+			printf "${BOLD}${RESET_COLOR}Delete branch${GREEN} "$current_branch"${RESET_COLOR} ? (y/n) ${RESET}"
+			read delete_branch
+			if [ "$delete_branch" = "y" ]; then
+				# TODO : Remote branch Deletion
+				check_delete_remote_branch() {
+					if [ "$current_branch" = "$default_branch" ]; then
+						echo "${BOLD}${RESET_COLOR} Fatal ! Cannot Delete the Default Branch "
+					else
+						printf "${BOLD}${RESET_COLOR}Delete remote branch${GREEN} "$current_branch"${RESET_COLOR} ? (y/n) ${RESET}"
+						read delete_remote_branch
+						echo ${RESET}
+						if [ "$delete_remote_branch" = "y" ]; then
+							git push origin --delete "$current_branch"
+						elif [ "$delete_remote_branch" = "n" ]; then
+							return 0
+						else
+							check_delete_remote_branch
+						fi
+					fi
+				}
 
-								# Check for internet connectivity to GitHub
-								if $SUDO ping -c 1 github.com &>/dev/null; then
-									check_delete_remote_branch
-								fi
+				git checkout "$default_branch" >/dev/null 2>&1
+
+				if has_remote; then
+					repo_url=$(git config --get remote.origin.url)
+					repo_owner=$(echo "$repo_url" | awk -F '[/:]' '{print $(NF-1)}')
+
+					# check if we are not the owner of the repo
+					if [ "$repo_owner" == "$current_user" ]; then
+						is_remote_branch=$(git branch -r | grep "origin/$current_branch")
+						
+						if [ -n "$is_remote_branch" ]; then
+							# prompt for sudo
+							# password if required
+							allow_sudo
+
+							# Check for internet connectivity to GitHub
+							if $SUDO ping -c 1 github.com &>/dev/null; then
+								check_delete_remote_branch
 							fi
 						fi
 					fi
-					git branch -D "$current_branch"
-				elif [ "$delete_branch" = "n" ]; then
-					return 0
-				else
-					check_delete_branch
 				fi
-			}
-			check_delete_branch
-		fi
+				git branch -D "$current_branch"
+			elif [ "$delete_branch" = "n" ]; then
+				return 0
+			else
+				check_delete_branch
+			fi
+		}
+		check_delete_branch
 	else
 		echo "${BOLD}${RESET_COLOR} Usage : gbd branch_to_delete"
 	fi
